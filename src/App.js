7ec98e202bb546a4ad8663f79e17995c2a4e77ac
Shrish236 <shrishrajamohan@gmail.com>
2024-01-15 19:41:33 +0530

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+// import { Desktop } from './components/temp';
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Desktop/> */}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
-
+import logoURL from '../../assets/TN-logo.png'
+import '../styles.css'
 class Header extends Component {
+  
   render() {
-    return <div>Header</div>
+    return <div className="flex h-20 bg-gray-100 p-1">
+        <img src = {logoURL} className="m-2 h-16 self-center float-left" alt='logo'/>
+        <span className=''>
+          <span className="absolute ml-2 mt-3 w-35 float-left text-left text-lg font-medium">Department of Horticulture and Plantation Crops</span>
+          
+          <span className="absolute ml-2 mt-9 w-35 float-left text-left text-gray-500 text-base font-medium">Government of Tamil Nadu</span>
+        </span>
+        {/* <p className={'header-text'}>Department of Horticulture and Plantation Crops</p> */}
+        {/* <h1 class="text-3xl font-bold underline text-red-300"> */}
+        {/* Hello world! */}
+    {/* </h1> */}
+        </div>
   }
 }
 
